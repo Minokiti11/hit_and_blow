976dc249d636f20ca80ec0da24eeacbb7b0a2230
izukiti07 <izuu.3072@icloud.com>
2026-01-14 11:31:14 +0900

    iro

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
     char player[100];
@@ -10,15 +11,18 @@ typedef struct {
 
 int main() {
     int current_turn = 1; /* odd: Player1, even: Player2 */
-    guess g1, g2;
+    guess g1, g2; /*p1とp2の構造体の配列*/
+    guess *P1 = &g1;/*g1のポインタ*/
+    guess *P2 = &g2;/*g2のポインタ*/
+
     char player1_name[100];
     char player2_name[100];
     printf("Enter Player1's name: ");
-    scanf("%s", player1_name);
+    scanf("%s", player1_name);/*p1の名前*/
     printf("Enter Player2's name: ");
-    scanf("%s", player2_name);
+    scanf("%s", player2_name);/*p2の名前*/
 
-    /* Generate Answer for 4 digits */
+  
     printf("Generating Answer...\n");
 
     int answer[] = {}; /* ４桁のランダムな数字を生成 （同じ数字の桁がないように） */
